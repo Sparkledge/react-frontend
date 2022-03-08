00f1122a55bf58e3894022396234a9fe6f237a9c
SKupisz <szym-ku@wp.pl>
2022-03-08 09:14:46 +0100

    Started preparing signin panel for the connection with backend

--- src/components/subpages/signing.tsx
+++ src/components/subpages/signing.tsx
@@ -1,4 +1,5 @@
-import React, {Suspense} from "react";
+import React, {Suspense, useState} from "react";
+import axios from "axios";
 
 import { MainContainer, Preloader } from "../../styled/main";
 import { LandingSectionWrapper, LandingSectionFilter  } from "../../styled/subpages/welcome";
@@ -13,6 +14,20 @@ const BackgroundPattern = require("../../assets/pattern_background.webp");
 const FooterComponent = React.lazy(() => import("../helperComponents/welcome/footerComponent"))
 
 const SigningPanel:React.FC<SigningInterface> = ({mode}: SigningInterface) => {
+
+    const [Login, setLogin] = useState<string>("");
+    const [Password, setPassword] = useState<string>("");
+
+    const TriggerTheShot = () : void => {
+        if(Login.length !== 0 || Password.length !== 0){
+            const objectToSend = {
+                Login: Login,
+                Passwd: Password
+            };
+            //TODO: add connection with server after it is created
+        }
+    }
+
     return <MainContainer className="block-center">
         <Suspense fallback={<Preloader className="block-center">Ładowanie...</Preloader>}>
             <LandingSectionWrapper className="block-center" source={BackgroundPattern} backgroundSize="contain"
@@ -22,9 +37,10 @@ const SigningPanel:React.FC<SigningInterface> = ({mode}: SigningInterface) => {
                         {mode === 1 ? "Panel logowania" : "Panel rejestracji"}    
                     </AboutHeader>
                     <SigningPanelWrapper className="block-center">
-                        <SigningPanelInput className="block-center" type="text" placeholder="Login..."/>
+                        <SigningPanelInput className="block-center" type="text" placeholder="Login..."
+                            value={Login} onChange={(e) => setLogin(e.target.value)} required/>
                         <SigningPanelInput className="block-center" type="password" placeholder="Hasło..."
-                            marginBottom={20}/>
+                            marginBottom={20} value={Password} onChange={(e) => setPassword(e.target.value)} required/>
                         <SigningPanelButton className="block-center">
                             Zaloguj się    
                         </SigningPanelButton>
