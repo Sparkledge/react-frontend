b808e660cafa8b43f7a43ffc47de281f4c50d9e9
SKupisz <szym-ku@wp.pl>
2022-10-26 16:09:00 +0200

    Optimizing the performance of particular pages

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Suspense } from "react";
 import {
   BrowserRouter as Router, Routes, Route,
 } from "react-router-dom";
@@ -10,25 +10,42 @@ import useLocalStorage from "use-local-storage";
 import { LightMode, DarkMode, SparkledgeGlobalStyle } from "../styled/main";
 import { setNewToken } from "../redux/actions/generalActions";
 import { RootState } from "../redux/mainReducer";
+import refreshToken from "../connectionFunctions/main/refreshToken";
+import SearchingPreloaderComponent from "./helperComponents/searcher/searchingPreloaderComponent";
+
 import Navbar from "./subcomponents/navbar";
 import MemoryUsingBanner from "./subcomponents/memoryUsingBanner";
-import Welcome from "./subpages/welcome";
-import About from "./subpages/about";
-import SigningPanel from "./subpages/signing";
-import Profile from "./subpages/profile";
-import Searcher from "./subpages/searcher";
-import DocumentDisplayer from "./subpages/documentDisplayer";
-import DocumentUpload from "./subpages/documentUpload";
-import Notfound from "./subpages/notfound";
-import ForgotPassword from "./subpages/forgotPassword";
-import ResetPassword from "./subpages/resetPassword";
-import AuthenticationPanel from "./subpages/authentication";
-import TermsAndConditions from "./subpages/termsAndConditions";
-import FAQ from "./subpages/faq";
-import UserPanel from "./subpages/userPanel";
-import Settings from "./subpages/settings";
+// import Welcome from "./subpages/welcome";
+// import About from "./subpages/about";
+// import SigningPanel from "./subpages/signing";
+// import Profile from "./subpages/profile";
+// import Searcher from "./subpages/searcher";
+// import DocumentDisplayer from "./subpages/documentDisplayer";
+// import DocumentUpload from "./subpages/documentUpload";
+// import Notfound from "./subpages/notfound";
+// import ForgotPassword from "./subpages/forgotPassword";
+// import ResetPassword from "./subpages/resetPassword";
+// import AuthenticationPanel from "./subpages/authentication";
+// import TermsAndConditions from "./subpages/termsAndConditions";
+// import FAQ from "./subpages/faq";
+// import UserPanel from "./subpages/userPanel";
+// import Settings from "./subpages/settings";
 
-import refreshToken from "../connectionFunctions/main/refreshToken";
+const Welcome = React.lazy(() => import ("./subpages/welcome"));
+const About = React.lazy(() => import ("./subpages/about"));
+const SigningPanel = React.lazy(() => import ("./subpages/signing"));
+const Profile = React.lazy(() => import ("./subpages/profile"));
+const Searcher = React.lazy(() => import ("./subpages/searcher"));
+const DocumentDisplayer = React.lazy(() => import ("./subpages/documentDisplayer"));
+const DocumentUpload = React.lazy(() => import ("./subpages/documentUpload"));
+const Notfound = React.lazy(() => import ("./subpages/notfound"));
+const ForgotPassword = React.lazy(() => import ("./subpages/forgotPassword"));
+const ResetPassword = React.lazy(() => import ("./subpages/resetPassword"));
+const AuthenticationPanel = React.lazy(() => import ("./subpages/authentication"));
+const TermsAndConditions = React.lazy(() => import ("./subpages/termsAndConditions"));
+const FAQ = React.lazy(() => import ("./subpages/faq"));
+const UserPanel = React.lazy(() => import ("./subpages/userPanel"));
+const Settings = React.lazy(() => import("./subpages/settings"));
 
 ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
 
@@ -60,31 +77,35 @@ const Main: React.FC = () => {
   return (
     <ThemeProvider theme={graphicalMode === 0 ? LightMode : DarkMode}>
       <SparkledgeGlobalStyle isLight={graphicalMode === 0} />
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/signin" element={<SigningPanel mode={1} />} />
-          <Route path="/signup" element={<SigningPanel mode={2} />} />
-          <Route path="/searcher" element={<Searcher />} />
-          <Route path="/searcher/:courseId" element={<Searcher />} />
-          <Route path="/document/:docId" element={<DocumentDisplayer />} />
-          <Route path="/document/" element={<DocumentDisplayer />} />
-          <Route path="/documentUpload" element={<DocumentUpload />} />
-          <Route path="/panel" element={<UserPanel />} />
-          <Route path="/forgotPassword" element={<ForgotPassword />} />
-          <Route path="/resetPassword/:email/:token" element={<ResetPassword />} /> 
-          <Route path="/authentication/:token" element={<AuthenticationPanel />} />
-          <Route path="/terms" element={<TermsAndConditions />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/profile/:userId" element={<Profile />} />
-          <Route path="/profile/" element={<Profile />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="*" element={<Notfound />} />
-        </Routes>
-        {!isUserConsent ? <MemoryUsingBanner toggleIsUserConsent={toggleIsUserConsent} /> : null}
-      </Router>
+
+      <Suspense fallback={<SearchingPreloaderComponent />}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/signin" element={<SigningPanel mode={1} />} />
+            <Route path="/signup" element={<SigningPanel mode={2} />} />
+            <Route path="/searcher" element={<Searcher />} />
+            <Route path="/searcher/:courseId" element={<Searcher />} />
+            <Route path="/document/:docId" element={<DocumentDisplayer />} />
+            <Route path="/document/" element={<DocumentDisplayer />} />
+            <Route path="/documentUpload" element={<DocumentUpload />} />
+            <Route path="/panel" element={<UserPanel />} />
+            <Route path="/forgotPassword" element={<ForgotPassword />} />
+            <Route path="/resetPassword/:email/:token" element={<ResetPassword />} /> 
+            <Route path="/authentication/:token" element={<AuthenticationPanel />} />
+            <Route path="/terms" element={<TermsAndConditions />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/profile/:userId" element={<Profile />} />
+            <Route path="/profile/" element={<Profile />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="*" element={<Notfound />} />
+          </Routes>
+          {!isUserConsent ? <MemoryUsingBanner toggleIsUserConsent={toggleIsUserConsent} /> : null}
+        </Router>
+
+      </Suspense>
     </ThemeProvider>
   );
 };
