71b94d04986de9352346316eda1224efcde6bb87
SKupisz <szym-ku@wp.pl>
2022-12-06 15:45:17 +0100

    Improved the FAQ page, corrected the RWD of the user panel

--- src/components/subpages/faq.tsx
+++ src/components/subpages/faq.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { FAQHeader, FAQDescription, QuestionsContainer } from "src/styled/subpages/faq";
 
 import Template from "src/components/subcomponents/template";
@@ -8,20 +8,24 @@ import QuestionComponent from "src/components/helperComponents/faq/questionCompo
 
 import FAQList, { FAQQuestionType } from "src/data/faq";
 
-const FAQ:React.FC = () => (
-  <Template headTagTitle="FAQ - Sparkledge" headTagDesc="Tutaj możesz dowiedzieć się co nieco o naszej aplikacji" fallbackComponent={<SearchingPreloaderComponent />}>
-    <FAQHeader className="block-center">FAQ section</FAQHeader>
-    <FAQDescription className="block-center">
-      Kliknij na pytanie by wyświetlić odpowiedź
-    </FAQDescription>
-    <QuestionsContainer className="block-center">
-      {FAQList.map((elem: FAQQuestionType) => (
-        <QuestionComponent header={elem.header}>
-          {elem.answer}
-        </QuestionComponent>
-      ))}
-    </QuestionsContainer>
-  </Template>
-);
+const FAQ:React.FC = () => {
+  const Questions = useMemo(() => FAQList.map((elem: FAQQuestionType) => (
+    <QuestionComponent header={elem.header}>
+      {elem.answer}
+    </QuestionComponent>
+  )), [FAQList]);
+
+  return (
+    <Template headTagTitle="FAQ - Sparkledge" headTagDesc="Tutaj możesz dowiedzieć się co nieco o naszej aplikacji" fallbackComponent={<SearchingPreloaderComponent />}>
+      <FAQHeader className="block-center">FAQ section</FAQHeader>
+      <FAQDescription className="block-center">
+        Kliknij na pytanie by wyświetlić odpowiedź
+      </FAQDescription>
+      <QuestionsContainer className="block-center">
+        {Questions}
+      </QuestionsContainer>
+    </Template>
+  );
+};
 
 export default FAQ;
