32ef44077ed0f8a45337ea7214760176e5145a63
Bartosz S <01169625@pw.edu.pl>
2022-12-15 19:51:51 +0100

    added wrapper to NavbarItem & added functionality to hamburger menu

--- src/components/subcomponents/navbar.tsx
+++ src/components/subcomponents/navbar.tsx
@@ -17,18 +17,35 @@ const NavbarLogo = require("src/assets/sparkledge_logo.webp");
 // TODO: extract constants from styled-components
 // TODO: move styled-components to separate files
 // TODO: solve problem with black square below navbar
-// TODO: find way to place menu expand button in top right corner
 // TODO: make sure this is responsive
+// TODO: block scrolling when menu is expanded
+// TODO: remove break in navbar when in hamurger mode
 // TODO: ...
 
-const NavbarItemLink = styled(Link)`
+const NavbarItemWrapper = styled.div`
+  box-sizing: border-box;
+  padding: 0.3rem 0.6rem;
+
+  // to remove
+  border: 1px solid lime;
+
+  min-height: 100%;
+  @media screen and (max-width: 1000px) {
+    min-height: 5rem;
+  }
+
+  display: inline-flex;
+  flex-direction: row;
+  align-items: center;  
+`;
+
+const NavbarItem = styled.div`
   box-sizing: border-box;
 
   border: 1px solid transparent;
   border-radius: 16px;
-  background: transparent;
 
-  height: 100%;
+  /* height: 100%; */
   padding: 0.8rem 1.2rem;
   
   display: inline-flex;
@@ -39,32 +56,32 @@ const NavbarItemLink = styled(Link)`
   text-decoration: none;
   font-family: ${(props) => props.theme.fonts.main};
   color: ${(props) => props.theme.color};
-
   font-size: 1.2rem;
   white-space: nowrap;
 
   transition: 0.2s all;
+
+  background: transparent;
 
   &:hover {
     background: ${(props) => props.theme.hoverBgPrimary};
   }
 `;
 
-const NavbarItemButton = styled(NavbarItemLink)`
+const NavbarItemButton = styled.button`
   cursor: pointer;
 `;
 
-const NavbarExpandButton = styled(NavbarItemLink)`
+const NavbarExpandButton = styled.button`
   cursor: pointer;
+
+  height: fit-content;
 
   display: none;
   @media screen and (max-width: 1000px) {
     display: inline-block;
+    margin-left: auto;
   }
-
-  position: sticky;
-  top: 0;
-  right: 0;
 `;
 
 const NavbarImg = styled.img`
@@ -80,26 +97,29 @@ type NavbarProps = {
   isOpened: boolean;
 };
 
-const NavbarContainer2 = styled.div<NavbarProps>`
+const NavbarContainer = styled.div<NavbarProps>`
   box-sizing: border-box;
 
   height: 5rem;
   width: 100%;
+  padding-inline: 0.4rem;
 
-  padding: 0.4rem 0.8rem;
-
+  // to remove
   border: 2px solid blue;
 
   @media screen and (max-width: 1000px) {
     flex-direction: column;
-    height: ${(props) => props.isOpened ? "fit-content" : "5rem"};
+    height: ${(props) => props.isOpened ? "100vh" : "5rem"};
+    overflow: ${(props) => props.isOpened ? "scroll" : "hidden"};
+    gap: 0;
+    scroll-behavior: contain;
   }
 
   display: flex;
   flex-direction: row;
   gap: 0.5rem;
 
-  position: sticky;
+  position: fixed;
   top: 0;
 
   // this needs to change at some point
@@ -126,41 +146,69 @@ const Navbar:React.FC = () => {
     setIsMenuExpanded(!isMenuExpanded);
   };
 
+  const handleMenuHidden = () => {
+    setIsMenuExpanded(false);
+  };
+
   return (
-    <NavbarContainer2 isOpened={isMenuExpanded}>
-      <NavbarExpandButton as="button" onClick={handleMenuExpanded}>
-        =
-      </NavbarExpandButton>
+    <NavbarContainer isOpened={isMenuExpanded}>
+      <NavbarItemWrapper>
+        <NavbarItem as={Link} to="/">
+          LOGO
+        </NavbarItem>
 
-      <NavbarItemLink to="/">
-        <NavbarImg src={NavbarLogo} alt="sparkledge logo" />
-      </NavbarItemLink>
+        <NavbarItem as={NavbarExpandButton} onClick={handleMenuExpanded}>
+          {isMenuExpanded ? "x" : "="}
+        </NavbarItem>
+      </NavbarItemWrapper>
 
-      <NavbarItemLink to="/">
-        wyszukiwarka
-      </NavbarItemLink>
+      <NavbarItemWrapper>
+        <NavbarItem as={Link} to="/" onClick={handleMenuHidden}>
+          wyszukiwarka
+        </NavbarItem>
+      </NavbarItemWrapper>
 
       <NavbarItemBreak />
 
-      <NavbarItemLink to="/">
-        o nas
-      </NavbarItemLink>
+      <NavbarItemWrapper>
+        <NavbarItem as={Link} to="/faq" onClick={handleMenuHidden}>
+          o nas
+        </NavbarItem>
+      </NavbarItemWrapper>
 
-      <NavbarItemLink to="/">
-        zaloguj się
-      </NavbarItemLink>
+      <NavbarItemWrapper>
+        <NavbarItem as={Link} to="/" onClick={handleMenuHidden}>
+          zarejestruj się
+        </NavbarItem>
+      </NavbarItemWrapper>
 
-      <NavbarItemLink to="/">
-        zarejestruj się
-      </NavbarItemLink>
+      <NavbarItemWrapper>
+        <NavbarItem as={Link} to="/" onClick={handleMenuHidden}>
+          zaloguj się
+        </NavbarItem>
+      </NavbarItemWrapper>
 
-      <NavbarItemButton as="button" onClick={handleThemeChanged}>
-        {graphicalMode
-          ? <LightModeIcon style={{ fontSize: "inherit", height: "inherit" }} />
-          : <DarkModeIcon style={{ fontSize: "inherit", height: "inherit" }} />}
-      </NavbarItemButton>
+      <NavbarItemWrapper>
+        <NavbarItem as={Link} to="/" onClick={handleMenuHidden}>
+          TEST
+        </NavbarItem>
+      </NavbarItemWrapper>
 
-    </NavbarContainer2>
+      <NavbarItemWrapper>
+        <NavbarItem as={Link} to="/" onClick={handleMenuHidden}>
+          TEST
+        </NavbarItem>
+      </NavbarItemWrapper>
+
+      <NavbarItemWrapper>
+        <NavbarItem as={NavbarItemButton} onClick={() => { handleThemeChanged(); handleMenuHidden(); }}>
+          {graphicalMode
+            ? <LightModeIcon style={{ fontSize: "inherit", height: "inherit" }} />
+            : <DarkModeIcon style={{ fontSize: "inherit", height: "inherit" }} />}
+        </NavbarItem>
+      </NavbarItemWrapper>
+
+    </NavbarContainer>
   );
 };
 
